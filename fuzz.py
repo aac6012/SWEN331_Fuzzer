1455414d923e42ead894541839c3de08910e6a06
Austin Cook <aac6012@rit.edu>
2015-09-22 13:15:18 -0400

    Requests is now locally installed to the project.   Added some code for page discovery.

--- fuzz.py
+++ fuzz.py
@@ -43,11 +43,58 @@ def getArgs(passed_args):
     return [mode, url, parsed_options[0]]
 
 
+# This is the main method run during execution
 def main(passed_args):
+    print()
     args = getArgs(passed_args)
-    print('Mode: ', args[0])
-    print('URL: ', args[1])
-    print('Options: ', args[2])
+    if args[0] == 'discover':
+        discover(args[1], args[2:])
+    elif args[0] == 'fuzz':
+        fuzz(args[1], args[2:])
+    else: # This else is somewhat redundant.  Mode input is verified in getArgs.
+        print('Not a valid <mode> param.  Must be \'discover\' or \'test\'')
+        sys.exit(0)
+
+def discover(url, args):
+    # Needed for discover:
+    # - Custom authentication
+    # - Page discovery: (Link discovery and Page guessing)
+    discovered = []
+    discoverPages(url, '', discovered)
+    print('DISCOVERED LINKS:\n', discovered)
+    # - Input discovery: (Parse URLs, Form Parameters, Cookies)
+    pass
+
+# This will be a recursive function to discover/process all pages.
+def discoverPages(base_url, current_url, visited):
+    visited.append(base_url + current_url)
+    current_links = discoverPagesCurrent(base_url, current_url)
+    for link in current_links:
+        if (base_url + link) not in visited:
+            discoverPages(base_url, link, visited)
+
+def discoverPagesCurrent(base_url, url):
+    r = requests.get(base_url + url)
+    lst = r.text.split(base_url)
+    lst = lst[1:]
+    for x in range(0, len(lst)):
+        # Get the element from the list
+        element = lst[x]
+        # Modify the element to remove tailing text
+        element = element[:element.find('\"')]
+        if not element.startswith('/'):
+            element = '/' + element
+
+        # Store the modified element back into the list
+        lst[x] = element
+    return lst
+
+
+
+
+
+def fuzz(url, args):
+    pass
 
 
 if __name__ == '__main__':
