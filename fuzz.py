dfed4a08e326a40985f55555ebc0c34759fae79c
Austin Cook <aac6012@rit.edu>
2015-09-23 01:04:41 -0400

    Redid argument parsing to since it was done incorrectly the first time.  Page guessing should now work for the fuzz discover.  Added a new file 'myWords.txt' which contains words to guess as url pages.

--- fuzz.py
+++ fuzz.py
@@ -17,78 +17,122 @@ class MyHTMLParser(HTMLParser):
             pass
 
 def getArgs(passed_args):
+    args = {}
     # Get the arguments passed from command line.
-    args = passed_args[1:]  # eliminate the running file from args.
+    passed_args = passed_args[1:]  # eliminate the running file from args.
     # Exit if there are not enough parameters
-    if len(args) < 2:
+    if len(passed_args) < 2:
         print('Not enough parameters: fuzz <mode> <url> <options>')
         sys.exit(0)
 
     # Get the mode argument.
-    mode = args[0]
+    args['mode'] = passed_args[0]
+    long_options = []
     # If invalid mode, output message and exit.
-    if not (mode == 'discover' or mode == 'test'):
+    if not (args['mode'] == 'discover' or args['mode'] == 'test'):
         print('Not a valid <mode> param.  Must be \'discover\' or \'test\'')
         sys.exit(0)
-
+    # Define the valid options
+    elif args['mode'] == 'discover':
+        long_options = ['custom-auth=', 'common-words=']
+    elif args['mode'] == 'test':
+        long_options = ['custom-auth=', 'vectors=', 'sensitive=', 'random=', 'slow=']
     # Get the url argument
-    url = args[1]
+    args['url'] = passed_args[1]
     # TODO (maybe): validate url input.
-    if not url:
+    if not args['url']:
         print('Not a valid url!')
         sys.exit(0)
 
     # Eliminate the mode and url to leave only options
-    args = args[2:]
-
-    # Define the valid options
-    long_options = ['custom-auth=', 'common-words=', 'vectors=', 'sensitive=', 'random=', 'slow=']
+    passed_args = passed_args[2:]
 
     # Parse the options, program will terminate if invalid option detected.
+    parsed_options = []
     try:
-        parsed_options = getopt.getopt(args, '', long_options)
+        parsed_options = getopt.getopt(passed_args, '', long_options)
     except getopt.GetoptError as e:
         print('\nInvalid option!', e.msg, sep='\n')
         sys.exit(0)
 
-    # Print the parsed options list of tuples.
-    return [mode, url, parsed_options[0]]
+    # Add the extracted options to the args dict
+    for opt in parsed_options[0]:
+        if (opt[0][2:] + '=') in long_options:
+            args[opt[0][2:]] = opt[1]
+
+    # Check that the required option args exist
+    if args['mode'] == 'discover':
+        keys = args.keys()
+        if 'common-words' not in keys:
+            print('Unable to run.\nDiscover requires the \'--common-words\' option to be specified.')
+            sys.exit(0)
+    elif args['mode'] == 'test':
+        keys = args.keys()
+        # Check for required args
+        if 'vectors' not in keys:
+            print('Unable to run.\nTest requires the \'--vectors\' option to be specified.')
+            sys.exit(0)
+        elif 'sensitive' not in keys:
+            print('Unable to run.\nTest requires the \'--sensitive\' option to be specified.')
+            sys.exit(0)
+        # Set non-required args to default if not specified.
+        if 'random' not in keys:
+            args['random'] = False
+        if 'slow' not in keys:
+            args['slow'] = 500
+
+    # Return the constructed args dict.
+    return args
 
 
 # This is the main method run during execution
 def main(passed_args):
-    print('\n----------------------------------------')
     args = getArgs(passed_args)
-    if args[0] == 'discover':
-        # args[1] contains the base url.
-        discover(args[1], args[2:])
-    elif args[0] == 'fuzz':
-        fuzz(args[1], args[2:])
+
+    if args['mode'] == 'discover':
+        discover(args)
+    elif args['mode'] == 'fuzz':
+        fuzz(args)
     else: # This else is somewhat redundant.  Mode input is verified in getArgs.
         print('Not a valid <mode> param.  Must be \'discover\' or \'test\'')
         sys.exit(0)
 
-def discover(url, args):
+def discover(args):
     # Needed for discover:
     # - Custom authentication
     # - Page discovery: (Link discovery and Page guessing)
-    discovered_pages = []
-    discoverPages(url, discovered_pages)
-    print('DISCOVERED LINKS:\n', discovered_pages)
+    crawled_pages = []
+    crawlPages(args['url'], crawled_pages)
+    print('','-'*50, 'CRAWLED LINKS:', '-'*50, sep='\n')
+    for page in crawled_pages:
+        print(page)
+    print('\n')
+    # Page guessing
+    guessed_pages = guessPages(args['url'], args['common-words'])
+    print('-'*50, 'GUESSED LINKS:', '-'*50, sep='\n')
+    for page in guessed_pages:
+        print(page)
+    print('\n')
+
     # - Input discovery: (Parse URLs, Form Parameters, Cookies)
-    discovered_input = discoverInput(discovered_pages)
-    print('DISCOVERED INPUTS:\n', discovered_input)
+    discovered_input = discoverInput(crawled_pages)
+    print('-'*50, 'DISCOVERED INPUTS:', '-'*50, sep='\n')
+    for key in discovered_input.keys():
+        print(key, ':')
+        for val in discovered_input[key]:
+            print(val)
+        print('-'*10)
 
 # This will be a recursive function to discover/process all pages.
-def discoverPages(url, visited):
+def crawlPages(url, visited):
     visited.append(url)
-    current_links = discoverPagesCurrent(url)
+    current_links = crawlPagesCurrent(url)
     for link in current_links:
         if link not in visited:
-            discoverPages(link, visited)
+            crawlPages(link, visited)
 
 # This will parse and return the current url's html file to extract links to other pages.
-def discoverPagesCurrent(url):
+def crawlPagesCurrent(url):
     # instantiate the parser
     parser = MyHTMLParser()
     # make the request to the url
@@ -106,17 +150,43 @@ def discoverPagesCurrent(url):
         # The base url will be the same regardless of the parameters passed, so eliminate the parameters from the link.
         if params_index != -1:
             links[x] = links[x][:params_index]
+        #Remove the link if it leads to a 404
+        if r.status_code == 404:
+            links[x] = url
 
     # add the url returned from the request, in case the url parameter is different.
     links.append(r.url)
     return links
 
+def guessPages(base_url, filename):
+    guessed_pages = []
+    possible_endings = ['', '.php', '.txt', '.html', '.jsp']
+    # Load the file of common words
+    f = open(filename, 'r')
+    # Each line is a single word
+    for line in f:
+        # Loop through possible url endings, guessing each.
+        for opt in possible_endings:
+            url = base_url + '/' + line.strip('\n') + opt
+            r = requests.get(url)
+            # If the page does not return a 404, then that page was successfully guessed.
+            if r.status_code != 404:
+                guessed_pages.append(url)
+
+    return guessed_pages
+
+
+
+
+
+
 def discoverInput(url_list):
-    inputs = []
+    inputs = {}
     for url in url_list:
+        inputs[url] = []
         for element in discoverInputCurrent(url):
-            if element not in inputs:
-                inputs.append(element)
+            if element not in inputs[url]:
+                inputs[url].append(element)
     return inputs
 
 def discoverInputCurrent(url):
@@ -130,7 +200,7 @@ def discoverInputCurrent(url):
     inputs = parser.found_input
     return inputs
 
-def fuzz(url, args):
+def fuzz(args):
     pass
 
 
