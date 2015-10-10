fa783beec2e6fe55fc475bfae38a196fa722e17d
Austin Cook <aac6012@rit.edu>
2015-10-10 18:27:56 -0400

    test.py: Testing now works for all vector types for form/url parameters.

--- test.py
+++ test.py
@@ -8,94 +8,112 @@ import requests.exceptions as exceptions
 
 #Test all of the possible vectors.
 def test(args, session):
+
     #Parse the values from the vectors file.
     vectors = parseVectors(args['vectors'])
+    #Get the dictionary return from running discover.
     discovered = discover.discover(args, session)
+    #Run the tests using the discovered information, list of vectors, and slow parameter.
+    run_tests(vectors, discovered, args['slow'])
 
-    xss(vectors['XSS'], discovered, args['slow'])
-    #bfo(vectors['BFO'])
-    #fse(vectors['FSE'])
-    #int(vectors['INT'])
-    #sqp(vectors['SQP'])
-    #sqi(vectors['SQI'])
-    #ldap(vectors['LDAP'])
-    #xpath(vectors['XPATH'])
-    #xml(vectors['XML'])
+    print('-'*50, 'Testing completed', '-'*50, sep='\n')
 
-#Cross-site scripting
-def xss(vector, discovered, slow):
-    print("Testing cross-site scripting")
+#Test the files
+def run_tests(vectors, discovered, slow):
 
-    #Uncomment this line will to cause a 404 error later on.
+    #Uncomment this line/lines to cause a 404 error later on.
     #discovered['formParams']['http://127.0.0.1/dvwa/404'] = [{'name': '404'}]
+    #discovered['urlParams']['http://127.0.0.1:8080/bodgeit/404/'] = ['test']
 
-    #Loop through each key in formParams, each key is a url
-    for url in discovered['formParams'].keys():
-        #Loop through each value in the vector list.
-        for value in vector:
-            #Construct payload to pass in request
-            payload = {}
-            for param in discovered['formParams'][url]:
-                payload[param.get('name')] = value
-            #Get the start time for the request.
-            start_time = time.time()
-            #Make the POST request to the url using the build payload.
-            response = requests.post(url, data=payload)
-            response.content
-            #Calculate the response time, using the current and start time.
-            response_time = (time.time() - start_time)
-            response_time = response_time * 1000
+    print('-'*50)
+    print('Testing form parameters.')
+    print('-'*50)
+    #Loop through each vectors category
+    for key in vectors.keys():
 
-            #Check for possible Denial of Service error from slow response.
-            if response_time > slow:
-                print("Slow response time, possible DOS!")
-                print("URL: ", url)
-                print("Params: ", payload)
-                print("Response time: ", response_time)
-                print()
-            #Check for error codes returned from the response object.
-            try:
-                response.raise_for_status()
-            except exceptions.HTTPError as e:
-                print(e)
-                print('URL: ', url)
-                print('Params: ', payload)
+        #Loop through each key in formParams, each key is a url
+        for url in discovered['formParams'].keys():
+            #Loop through each value in the vector list.
+            for value in vectors[key]:
+                #Construct payload to pass in request
+                payload = {}
+                for param in discovered['formParams'][url]:
+                    payload[param.get('name')] = value
+                #Get the start time for the request.
+                start_time = time.time()
+                #Make the POST request to the url using the build payload.
+                response = requests.post(url, data=payload)
+                response.content
+                #Calculate the response time, using the current and start time.
+                response_time = (time.time() - start_time)
+                response_time = response_time * 1000
 
+                #Check for possible Denial of Service error from slow response.
+                if response_time > slow:
+                    print('Slow response time, possible DOS!')
+                    print('Vector type: ', key)
+                    print('URL: ', url)
+                    print('Params: ', payload)
+                    print('Response time: ', response_time)
+                    print('-----') # Add a line for readability
 
-
-#Buffer overflow
-def bfo(values):
-    print("Testing Buffer Overflow")
-
-#Format string errors
-def fse(values):
-    print("Testing format string errors")
-
-#Integer overflow
-def int(values):
-    print("Testing integer overflow")
-
-#Passive SQL Injection
-def sqp(values):
-    print("Testing passive SQL injection")
-
-#Active SQL Injection
-def sqi(values):
-    print("Testing active SQL injection")
-
-#LDAP Injection:
-def ldap(values):
-    print("Testing LDAP Injection")
-
-#XPATH injection
-def xpath(values):
-    print("Testing XPATH Injection")
-
-#XML Injection
-def xml(values):
-    print("Testing XML Injection")
+                #Check for error codes returned from the response object.
+                try:
+                    response.raise_for_status()
+                except exceptions.HTTPError as e:
+                    print(e)
+                    print('URL: ', url)
+                    print('Params: ', payload)
+                    print('Vector type: ', key)
+                    print('-----') # Add a line for readability
 
 
+    print('-'*50)
+    print('Testing URL Parameters...')
+    print('-'*50)
+    for key in vectors.keys():
+
+        #Loop through each key in formParams, each key is a url
+        for url in discovered['urlParams']:
+            #Loop through each value in the vector
+            for value in vectors[key]:
+                # Create the params to pass in through the request.
+                params = {}
+                # Build the params
+                for param in discovered['urlParams'][url]:
+                    params[param] = value
+
+                #Get the start time for the request.
+                start_time = time.time()
+                #Make the GET request to the url using the built url params.
+                response = requests.get(url, params=params)
+                response.content
+                #Calculate the response time, using the current and start time.
+                response_time = (time.time() - start_time)
+                response_time = response_time * 1000
+
+                #Check for possible Denial of Service error from slow response.
+                if response_time > slow:
+                    print('Slow response time, possible DOS!')
+                    print('Vector type: ', key)
+                    print('URL: ', url)
+                    print('Params: ', params)
+                    print('Response time: ', response_time)
+                    print('-----') # Add a line for readability
+
+                #Check for error codes returned from the response object.
+                try:
+                    response.raise_for_status()
+                except exceptions.HTTPError as e:
+                    print(e)
+                    print('Params: ', params)
+                    print('Vector type: ', key)
+                    print('-----') # Add a line for readability
+
+
+
+
+#Parse the vectors file into a dictionary of lists.
 def parseVectors(file):
     #Create the dictionary of values to return
     vectors = dict(XSS=[], BFO=[], FSE=[], INT=[], SQP=[], SQI=[], LDAP=[], XPATH=[], XML=[])
